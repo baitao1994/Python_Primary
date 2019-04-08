93f1b6c308b933e42a802b49e382b4dc9051a1fb
白韬 <48074691+baitao1994@users.noreply.github.com>
2019-04-08 10:54:56 +0800

    Update TempConvert.py

--- 认识/TempConvert.py
+++ 认识/TempConvert.py
@@ -1,7 +1,7 @@
 #TempConcert.py
 val = input("请输入待温度表示符号的温度值（例如：32C）：")
 if val[-1] in ['C','c']:
-    f = 1.8 * eval(val[0:-1]) + 32 # eval()是返回 表达式 的值，表达式可以是值也可以是字符串
+    f = 1.8 * eval(val[0:-1]) + 32 # eval()是返回 表达式 的值，表达式可以是值也可以是字符串，可int也可以是float
     print("转换后的温度为：%.2fF"%f)
 elif val[-1] in ['F','f']:
     c = (float(val[0:-1]) -32)/1.8 # 将整数或字符串转化为浮点数
